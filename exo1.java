56e4a88041be482fbe1deb9b097a1f0286f4c9e5
daviddiema7 <nozzytb@gmail.com>
2025-05-23 10:42:52 +0200

    ajout de la division et du test de cette divison

--- exo1.java
+++ exo1.java
@@ -3,9 +3,21 @@ import org.junit.jupiter.api.Test;
 
 public class CalculatriceTest {
 
+
     Calculatrice calc = new Calculatrice();
 
     @Test
     public void testAdd() {
         assertEquals(5.0f, calc.add(2.0f, 3.0f));
     }
+
+      @Test
+    public void testDiv() {
+        assertEquals(2.0f, calc.div(6.0f, 3.0f));
+    }
+
+    @Test
+    public void testDivByZero() {
+        assertThrows(ArithmeticException.class, () -> calc.div(6.0f, 0));
+    }
+}
